2ed264b09cb7043f0d0b1fb969611bcb9f7ad46c
Youngman Jo <youngman7.jo@gmail.com>
2025-07-29 02:22:15 +0900

    add bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,24 @@ import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitest.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
+  const plugins = [
+    react(),
+    mode === "analyze" &&
+      visualizer({
+        filename: "stats.html",
+        brotliSize: true,
+        gzipSize: true,
+      }),
+  ].filter(Boolean);
+
   return {
-    plugins: [react()],
+    plugins,
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
@@ -23,7 +34,7 @@ export default defineConfig(({ mode }) => {
       coverage: {
         enabled: true,
         include: ["src/**/*"],
-        exclude: ["src/main.tsx"],
+        exclude: ["src/main.tsx", "src/styles/theme.ts", "src/vite-env.d.ts"],
         thresholds: {
           lines: 80,
           functions: 80,
